ff1ae73c61ee95a41e8bb1eef8f3eb0d20e05355
nedahmadi <nedahmadi83@gmail.com>
2022-03-09 12:34:05 +0330

    Add api dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [Keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`searching for ${Keyword} definition`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${Keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordsearch(event) {

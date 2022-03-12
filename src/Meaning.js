70e8fa92be0d7af668ecd30d0ebd6ffee0122701
nedahmadi <nedahmadi83@gmail.com>
2022-03-12 13:18:37 +0330

    Added synonym and phonetic

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3> {props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
+            <h6>
               <br />
-              <em> EXAMPLE : {definition.example}</em>
-            </p>
+              <strong> Definition : </strong> {definition.definition}
+              <br />
+              <strong> EXAMPLE : </strong>
+              <br />
+              <em> {definition.example}</em>
+              <br />
+              <Synonyms synonym={definition.synonym} />
+            </h6>
           </div>
         );
       })}
